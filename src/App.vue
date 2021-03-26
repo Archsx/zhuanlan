1687bc776a763827c4c4ad3d06b27651d6de4132
Archsx <871728578@qq.com>
2021-03-26 12:47:53 +0800

    初步完成ColumnList组件

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,61 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="container">
+    <column-list :list="testData"></column-list>
   </div>
-  <router-view/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script lang="ts">
+import { defineComponent } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList from './components/ColumnList.vue'
+import { ColumnProps } from './types/column-list'
+const testData: Array<ColumnProps> = [
+  {
+    id: 1,
+    title: 'test1专栏',
+    description: '这是test1专栏的简介',
+    avatar: 'https://i.loli.net/2021/03/25/OxNmBTtkbHYyG3o.jpg'
+  },
+  {
+    id: 2,
+    title: 'test2专栏',
+    description: '这是test2专栏的简介',
+    avatar: 'https://i.loli.net/2021/03/25/OxNmBTtkbHYyG3o.jpg'
+  },
+  {
+    id: 3,
+    title: 'test3专栏',
+    description: '这是test3专栏的简介',
+    avatar: 'https://i.loli.net/2021/03/25/OxNmBTtkbHYyG3o.jpg'
+  },
+  {
+    id: 4,
+    title: 'test4专栏',
+    description: '这是test4专栏的简介',
+    avatar: 'https://i.loli.net/2021/03/25/OxNmBTtkbHYyG3o.jpg'
+  },
+  {
+    id: 5,
+    title: 'test5专栏',
+    description: '这是test5专栏的简介',
+    avatar: 'https://i.loli.net/2021/03/25/OxNmBTtkbHYyG3o.jpg'
+  },
+  {
+    id: 6,
+    title: 'test6专栏',
+    description: '这是test6专栏的简介',
+    avatar: 'https://i.loli.net/2021/03/25/OxNmBTtkbHYyG3o.jpg'
+  }
+]
+export default defineComponent({
+  name: '',
+  components: { ColumnList },
+  setup() {
+    return {
+      testData
     }
   }
-}
-</style>
+})
+</script>
+
+<style lang="" scoped></style>
---
+++ src/components/ColumnList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="row">
+    <div v-for="column in list" :key="column.id" class="col-4">
+      <img :src="column.avatar" :alt="column.title" />
+      <h5>{{ column.title }}</h5>
+      <p>{{ column.description }}</p>
+      <a href="#">进入专栏</a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { ColumnProps } from '../types/column-list'
+export default defineComponent({
+  name: 'ColumnList',
+  components: {},
+  props: {
+    list: {
+      type: Array as PropType<Array<ColumnProps>>,
+      required: true
+    }
+  },
+  setup(props) {
+    return {}
+  }
+})
+</script>
+
+<style lang="" scoped></style>
