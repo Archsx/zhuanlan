8091e48c163d9257ce008ba5ee46f3bbd9c1c864
Archsx <871728578@qq.com>
2021-05-16 16:19:24 +0800

    修改icode

--- src/service/base.ts
+++ src/service/base.ts
@@ -1,18 +1,19 @@
 import store from '@/store'
 import axios, { AxiosRequestConfig } from 'axios'
 axios.defaults.baseURL = 'http://apis.imooc.com/api/'
+const icode = '22848D414CEB7D8C'
 axios.interceptors.request.use(config => {
   store.commit('setLoading', true)
   store.commit('setError', { status: false, message: '' })
   // get 请求，添加到 url 中
-  config.params = { ...config.params, icode: '6461F2CAC3BD6261' }
+  config.params = { ...config.params, icode }
   // 其他请求，添加到 body 中
   // 如果是上传文件，添加到 FormData 中
   if (config.data instanceof FormData) {
-    config.data.append('icode', '6461F2CAC3BD6261')
+    config.data.append('icode', icode)
   } else {
     // 普通的 body 对象，添加到 data 中
-    config.data = { ...config.data, icode: '6461F2CAC3BD6261' }
+    config.data = { ...config.data, icode }
   }
 
   return config
